0b9990a23ef65dc206a06330063ddd80c7a0655d
Márcia Cristina da Silva Santos <marcia.cssantos95@gmail.com>
2021-02-01 16:03:08 -0300

    Trabalho pronto, porem a ser incrementado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Form, Input, Button } from 'reactstrap'
+import { Form, FormGroup, Jumbotron, Input, Button, ListGroup, ListGroupItem } from 'reactstrap'
 import Menu from './components/menu'
+import fundo from './img/maquete-de-conceito-de-fruta-deliciosa.jpg'
+import "./index.css"
 function App(){
   const [inputFrutas, setInputFrutas] = React.useState("")
   const [inputTitulo, setInputTitulo] = React.useState("")
 
   const frutas = useSelector((state) => state.reducer.frutas)
   const titulo = useSelector((state) => state.tituloReducer.titulo)  //Como não é uma array não precisa passar o .frutas
-  const quantidade = useSelector((state) => state.reducer2.quantidade)
   
   const dispatch = useDispatch();
 // Inicio da funcao que adiciona a fruta na pagina
@@ -24,49 +25,50 @@ function App(){
   }
   // Fim
 
-  function adicionarQuantidade(event){
-    event.preventDefault();
-
-    const objQtd = {
-      nome: inputQtd
-    }
-    dispatch({type: "ACRESCENTAR", value: objQtd})
-  }
-
   function alterarTitulo(event){
     setInputTitulo(event.target.value)
     dispatch({type: "ALTERAR", value: event.target.value})
   }
 
   return(
-    <div>
-      <Menu />
-      <Form>
-        <label>Título</label>
-        <Input 
-        placeholder="Informe o titulo" 
-        value={inputTitulo}
-        onChange = {alterarTitulo}
-        />
-      </Form>
+    <>
+    <img src={fundo}/>
+    <Jumbotron className="formulario">
+        <FormGroup>
+          
+          <Input 
+          placeholder="Informe o titulo" 
+          value={inputTitulo}
+          onChange = {alterarTitulo}
+          />
+        </FormGroup>
+        
+        <h1>{titulo}</h1>
+        <Form onSubmit = {adicionarFruta}>
+          <Input 
+            placeholder="Digite uma fruta..." 
+            value={inputFrutas} 
+            onChange={(event) => setInputFrutas(event.target.value)}
+          />
+          <Button className="botao" color="primary">
+            Enviar
+          </Button>
+        </Form>
+    </Jumbotron>
       
-      <h1>{titulo}</h1>
-      <Form onSubmit = {adicionarFruta}>
-        <Input 
-          placeholder="Digite uma fruta..." 
-          value={inputFrutas} 
-          onChange={(event) => setInputFrutas(event.target.value)}
-        />
-        <Button color="primary">
-          Enviar
-        </Button>
-      </Form>
-      {frutas.map((fruta, index) => {
-        return (
-          <li key = {index}>{fruta.nome}</li>
-        )
-      })}
-    </div>
+    <div className="lista">
+        <ListGroup>
+        {frutas.map((fruta, index) => {
+          return (
+            <ListGroupItem key = {index}>{fruta.nome}</ListGroupItem>
+          )
+        })}
+        </ListGroup>
+      </div>
+      
+      
+    
+    </>
   )
 }
 
